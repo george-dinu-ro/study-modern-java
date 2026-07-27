7f0bfa88e608ace7362c47c59ca0e4b563edf7f0
george-dinu-ro <george.dinu.ro@gmail.com>
2026-07-27 03:17:49 +0300

    add predicate example

---
+++ src/test/java/my/work/lambda/PersonStorage.java
@@ -0,0 +1,52 @@
+package my.work.lambda;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class PersonStorage {
+
+    public static Stream<Arguments> getPeople() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(
+                                Person.builder()
+                                        .name("John J.")
+                                        .age(20)
+                                        .phones(List.of("*10", "*11", "*12"))
+                                        .build(),
+                                Person.builder()
+                                        .name("Dan D.")
+                                        .age(25)
+                                        .phones(List.of("*21", "*22"))
+                                        .build(),
+                                Person.builder()
+                                        .name("Marry M.")
+                                        .age(30)
+                                        .phones(List.of("*31"))
+                                        .build(),
+                                Person.builder()
+                                        .name("Nick N.")
+                                        .age(27)
+                                        .phones(List.of("*41", "*42"))
+                                        .build(),
+                                Person.builder()
+                                        .name("Eugen E.")
+                                        .age(17)
+                                        .phones(List.of("*51"))
+                                        .build(),
+                                Person.builder()
+                                        .name("Larry L.")
+                                        .age(30)
+                                        .phones(Collections.emptyList())
+                                        .build(),
+                                Person.builder()
+                                        .name("Bob B.")
+                                        .age(16)
+                                        .phones(Collections.emptyList())
+                                        .build())));
+    }
+
+}
--- src/test/java/my/work/lambda/consumer/PersonConsumerTest.java
+++ src/test/java/my/work/lambda/consumer/PersonConsumerTest.java
@@ -3,11 +3,10 @@ package my.work.lambda.consumer;
 import my.work.ALogger;
 import my.work.lambda.Person;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -15,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 class PersonConsumerTest extends ALogger {
 
     @ParameterizedTest
-    @MethodSource("getPeople")
+    @MethodSource("my.work.lambda.PersonStorage#getPeople")
     void whenCallShowAllInfo_thenCallToStringMethod(List<Person> people) {
         PersonConsumer.showAllInfo(people);
 
@@ -23,24 +22,22 @@ class PersonConsumerTest extends ALogger {
                 "Person(name=John J., age=20, phones=[*10, *11, *12])",
                 "Person(name=Dan D., age=25, phones=[*21, *22])",
                 "Person(name=Marry M., age=30, phones=[*31])",
-                "Person(name=Nick N., age=27, phones=[*41, *42])"
-        );
+                "Person(name=Nick N., age=27, phones=[*41, *42])",
+                "Person(name=Eugen E., age=17, phones=[*51])",
+                "Person(name=Larry L., age=30, phones=[])",
+                "Person(name=Bob B., age=16, phones=[])");
 
-        assertEquals(4, getMessagesSize());
+        assertEquals(expected.size(), getMessagesSize());
 
-        assertEquals(expected.getFirst(), getMessage(0));
-
-        assertEquals(expected.get(1), getMessage(1));
-
-        assertEquals(expected.get(2), getMessage(2));
-
-        assertEquals(expected.getLast(), getMessage(3));
+        IntStream
+                .range(0, getMessagesSize())
+                .forEach(i -> assertEquals(expected.get(i), getMessage(i)));
 
         assertNotEquals(expected.getFirst() + System.currentTimeMillis(), getMessage(0));
     }
 
     @ParameterizedTest
-    @MethodSource("getPeople")
+    @MethodSource("my.work.lambda.PersonStorage#getPeople")
     void whenCallShowInfoSeparatelyWithConsumer_thenCall2Consumers(List<Person> people) {
         PersonConsumer.showInfoSeparatelyWithConsumer(people);
 
@@ -52,30 +49,25 @@ class PersonConsumerTest extends ALogger {
                 "Marry M.:30",
                 "*31",
                 "Nick N.:27",
-                "*41, *42"
-        );
+                "*41, *42",
+                "Eugen E.:17",
+                "*51",
+                "Larry L.:30",
+                "",
+                "Bob B.:16",
+                "");
 
-        assertEquals(8, getMessagesSize());
+        assertEquals(expected.size(), getMessagesSize());
 
-        assertEquals(expected.getFirst(), getMessage(0));
+        IntStream
+                .range(0, getMessagesSize())
+                .forEach(i -> assertEquals(expected.get(i), getMessage(i)));
 
-        assertEquals(expected.get(1), getMessage(1));
-
-        assertEquals(expected.get(2), getMessage(2));
-
-        assertEquals(expected.get(3), getMessage(3));
-
-        assertEquals(expected.get(4), getMessage(4));
-
-        assertEquals(expected.get(5), getMessage(5));
-
-        assertEquals(expected.get(6), getMessage(6));
-
-        assertEquals(expected.getLast(), getMessage(7));
+        assertNotEquals(expected.getFirst() + System.currentTimeMillis(), getMessage(0));
     }
 
     @ParameterizedTest
-    @MethodSource("getPeople")
+    @MethodSource("my.work.lambda.PersonStorage#getPeople")
     void whenCallShowInfoSeparatelyWithBiConsumer_thenCallBiConsumer(List<Person> people) {
         PersonConsumer.showInfoSeparatelyWithBiConsumer(people);
 
@@ -87,30 +79,25 @@ class PersonConsumerTest extends ALogger {
                 "Marry M.:30",
                 "*31",
                 "Nick N.:27",
-                "*41, *42"
-        );
+                "*41, *42",
+                "Eugen E.:17",
+                "*51",
+                "Larry L.:30",
+                "",
+                "Bob B.:16",
+                "");
 
-        assertEquals(8, getMessagesSize());
+        assertEquals(expected.size(), getMessagesSize());
 
-        assertEquals(expected.getFirst(), getMessage(0));
+        IntStream
+                .range(0, getMessagesSize())
+                .forEach(i -> assertEquals(expected.get(i), getMessage(i)));
 
-        assertEquals(expected.get(1), getMessage(1));
-
-        assertEquals(expected.get(2), getMessage(2));
-
-        assertEquals(expected.get(3), getMessage(3));
-
-        assertEquals(expected.get(4), getMessage(4));
-
-        assertEquals(expected.get(5), getMessage(5));
-
-        assertEquals(expected.get(6), getMessage(6));
-
-        assertEquals(expected.getLast(), getMessage(7));
+        assertNotEquals(expected.getFirst() + System.currentTimeMillis(), getMessage(0));
     }
 
     @ParameterizedTest
-    @MethodSource("getPeople")
+    @MethodSource("my.work.lambda.PersonStorage#getPeople")
     void whenCallShowInfoSeparatelyWithAgeFilter_thenCall2ConsumersWithAgeFilter(List<Person> people) {
         PersonConsumer.showInfoSeparatelyWithAgeFilter(people, 27);
 
@@ -118,42 +105,17 @@ class PersonConsumerTest extends ALogger {
                 "Marry M.:30",
                 "*31",
                 "Nick N.:27",
-                "*41, *42");
+                "*41, *42",
+                "Larry L.:30",
+                "");
 
-        assertEquals(4, getMessagesSize());
+        assertEquals(expected.size(), getMessagesSize());
 
-        assertEquals(expected.getFirst(), getMessage(0));
+        IntStream
+                .range(0, getMessagesSize())
+                .forEach(i -> assertEquals(expected.get(i), getMessage(i)));
 
-        assertEquals(expected.get(1), getMessage(1));
-
-        assertEquals(expected.get(2), getMessage(2));
-
-        assertEquals(expected.getLast(), getMessage(3));
-    }
-
-    private static Stream<Arguments> getPeople() {
-        return Stream.of(
-                Arguments.of(
-                        List.of(Person.builder()
-                                        .name("John J.")
-                                        .age(20)
-                                        .phones(List.of("*10", "*11", "*12"))
-                                        .build(),
-                                Person.builder()
-                                        .name("Dan D.")
-                                        .age(25)
-                                        .phones(List.of("*21", "*22"))
-                                        .build(),
-                                Person.builder()
-                                        .name("Marry M.")
-                                        .age(30)
-                                        .phones(List.of("*31"))
-                                        .build(),
-                                Person.builder()
-                                        .name("Nick N.")
-                                        .age(27)
-                                        .phones(List.of("*41", "*42"))
-                                        .build())));
+        assertNotEquals(expected.getFirst() + System.currentTimeMillis(), getMessage(0));
     }
 
     @Override
