1d4db5445d2d6c4a5387a8064ed30adf73df5aa0
george-dinu-ro <george.dinu.ro@gmail.com>
2026-07-28 09:10:30 +0300

    add stream map to person

---
+++ src/main/java/my/work/stream/map/PersonMap.java
@@ -0,0 +1,18 @@
+package my.work.stream.map;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import my.work.lambda.Person;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PersonMap {
+
+    public static List<String> getNames(List<Person> people) {
+        return people.stream()
+                .map(Person::getName)
+                .toList();
+    }
+
+}
---
+++ src/test/java/my/work/stream/map/PersonMapTest.java
@@ -0,0 +1,22 @@
+package my.work.stream.map;
+
+import my.work.lambda.Person;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+class PersonMapTest {
+
+    @ParameterizedTest
+    @MethodSource("my.work.lambda.PersonStorage#getPeople")
+    void whenCallGetNames_thenCallMap(List<Person> people) {
+        var expected = List.of("John J.", "Dan D.", "Marry M.", "Nick N.", "Eugen E.", "Larry L.", "Bob B.");
+        var actual = PersonMap.getNames(people);
+
+        assertIterableEquals(expected, actual);
+    }
+
+}
