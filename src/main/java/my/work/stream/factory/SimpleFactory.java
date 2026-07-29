5f84bdf4922f9104c744525436ced2a66eafc6d9
george-dinu-ro <george.dinu.ro@gmail.com>
2026-07-29 20:32:50 +0300

    small code refactoring

--- src/main/java/my/work/stream/factory/SimpleFactory.java
+++ src/main/java/my/work/stream/factory/SimpleFactory.java
@@ -4,22 +4,30 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SimpleFactory {
+
+    private static final UnaryOperator<String> FUNCTION = x -> String.valueOf((char) (x.charAt(0) + 1));
+
+    private static Supplier<String> supplier() {
+        var counter = new AtomicInteger('a');
+        return () -> String.valueOf((char) counter.getAndIncrement());
+    }
 
     public static Stream<String> of() {
         return Stream.of("a", "b", "c", "d", "e");
     }
 
     public static Stream<String> iterate() {
-        return Stream.iterate("a", x -> String.valueOf((char) (x.charAt(0) + 1))).limit(5);
+        return Stream.iterate("a", FUNCTION).limit(5);
     }
 
     public static Stream<String> generate() {
-        var counter = new AtomicInteger('a');
-        return Stream.generate(() -> String.valueOf((char) counter.getAndIncrement())).limit(5);
+        return Stream.generate(supplier()).limit(5);
     }
 
 }
