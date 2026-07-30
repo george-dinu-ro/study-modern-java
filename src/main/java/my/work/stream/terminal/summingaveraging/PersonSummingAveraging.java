d963989974389a27be0157569c1f9171fe1a5b09
george-dinu-ro <george.dinu.ro@gmail.com>
2026-07-30 22:47:30 +0300

    add stream summing and averaging example to person

---
+++ src/main/java/my/work/stream/terminal/summingaveraging/PersonSummingAveraging.java
@@ -0,0 +1,27 @@
+package my.work.stream.terminal.summingaveraging;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import my.work.Person;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PersonSummingAveraging {
+
+    private static int getNumberOfPhones(Person person) {
+        return person.getPhones().size();
+    }
+
+    public static int totalNumberOfPhones(List<Person> people) {
+        return people.stream()
+                .collect(Collectors.summingInt(PersonSummingAveraging::getNumberOfPhones));
+    }
+
+    public static double averageNumberOfPhones(List<Person> people) {
+        return people.stream()
+                .collect(Collectors.averagingDouble(PersonSummingAveraging::getNumberOfPhones));
+    }
+
+}
---
+++ src/test/java/my/work/stream/terminal/summingaveraging/PersonSummingAveragingTest.java
@@ -0,0 +1,31 @@
+package my.work.stream.terminal.summingaveraging;
+
+import my.work.Person;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonSummingAveragingTest {
+
+    @ParameterizedTest
+    @MethodSource("my.work.PersonStorage#getPeople")
+    void whenCallTotalNumberOfPhones_thenUseSummingInt(List<Person> people) {
+        var expected = 9;
+        var actual = PersonSummingAveraging.totalNumberOfPhones(people);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("my.work.PersonStorage#getPeople")
+    void whenCallAverageNumberOfPhones_thenUseAveragingDouble(List<Person> people) {
+        var expected = 1.28D;
+        var actual = PersonSummingAveraging.averageNumberOfPhones(people);
+
+        assertEquals(expected, actual, 0.006);
+    }
+
+}
