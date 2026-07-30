c526dd804bf618203df8c750b916c7ec63997e30
george-dinu-ro <george.dinu.ro@gmail.com>
2026-07-30 22:23:45 +0300

    add stream summing and averaging example

---
+++ src/main/java/my/work/stream/terminal/summingaveraging/SimpleSummingAveraging.java
@@ -0,0 +1,24 @@
+package my.work.stream.terminal.summingaveraging;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SimpleSummingAveraging {
+
+    public static int summingV1(List<String> input) {
+        return input.stream().collect(Collectors.summingInt(String::length));
+    }
+
+    public static int summingV2(List<String> input) {
+        return input.stream().mapToInt(String::length).sum();
+    }
+
+    public static double averaging(List<String> input) {
+        return input.stream().collect(Collectors.averagingDouble(String::length));
+    }
+
+}
---
+++ src/test/java/my/work/stream/terminal/summingaveraging/SimpleSummingAveragingTest.java
@@ -0,0 +1,48 @@
+package my.work.stream.terminal.summingaveraging;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleSummingAveragingTest {
+
+    @ParameterizedTest
+    @MethodSource("initSumming")
+    void whenCallSummingV1_thenUseSumming(List<String> input, int expected) {
+        var actual = SimpleSummingAveraging.summingV1(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("initSumming")
+    void whenCallSummingV2_thenUseMapToInt(List<String> input, int expected) {
+        var actual = SimpleSummingAveraging.summingV2(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("averagingSumming")
+    void whenCallAveraging_thenUseAveraging(List<String> input, double expected) {
+        var actual = SimpleSummingAveraging.averaging(input);
+
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> initSumming() {
+        return Stream.of(Arguments.of(
+                List.of("a", "aa", "aaa", "aaaa"), 10));
+    }
+
+    private static Stream<Arguments> averagingSumming() {
+        return Stream.of(Arguments.of(
+                List.of("a", "aa", "aaa", "aaaa"), 2.5D));
+    }
+
+}
