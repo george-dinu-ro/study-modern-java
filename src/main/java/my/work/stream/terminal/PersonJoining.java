34fe330b03c26ae4ababd360bb7e2fc01db4dea9
george-dinu-ro <george.dinu.ro@gmail.com>
2026-07-30 20:02:58 +0300

    add stream joining example to person

---
+++ src/main/java/my/work/stream/terminal/PersonJoining.java
@@ -0,0 +1,19 @@
+package my.work.stream.terminal;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import my.work.Person;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PersonJoining {
+
+    public static String getNames(List<Person> people) {
+        return people.stream()
+                .map(Person::getName)
+                .collect(Collectors.joining(", ", "(", ")"));
+    }
+
+}
---
+++ src/test/java/my/work/stream/terminal/PersonJoiningTest.java
@@ -0,0 +1,22 @@
+package my.work.stream.terminal;
+
+import my.work.Person;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonJoiningTest {
+
+    @ParameterizedTest
+    @MethodSource("my.work.PersonStorage#getPeople")
+    void whenCallGetNames_thenUseJoining(List<Person> people) {
+        var expected = "(John J., Dan D., Marry M., Nick N., Eugen E., Larry L., Bob B.)";
+        var actual = PersonJoining.getNames(people);
+
+        assertEquals(expected, actual);
+    }
+
+}
