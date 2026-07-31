472bb0c6127153ef0d5699c87ce9cbe519bf28f4
george-dinu-ro <george.dinu.ro@gmail.com>
2026-07-31 10:08:33 +0300

    add groupingBy and maxBy with 2 parameters to person

---
+++ src/main/java/my/work/stream/terminal/groupingBy/twoparameters/sort/PersonGroupByTwoWithDescOrder.java
@@ -0,0 +1,46 @@
+package my.work.stream.terminal.groupingBy.twoparameters.sort;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import my.work.Person;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PersonGroupByTwoWithDescOrder {
+
+    public static Map<String, Optional<Person>> groupingByStatusOrderByAgeDescOptional(List<Person> people) {
+        return people.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Person::getStatus,
+                                Collectors.maxBy(Comparator.comparingInt(Person::getAge))));
+    }
+
+    public static Map<String, Person> groupingByStatusOrderByAgeDescV1(List<Person> people) {
+        return people.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Person::getStatus,
+                                Collectors.collectingAndThen(
+                                        Collectors.maxBy(
+                                                Comparator.comparing(Person::getAge)),
+                                        Optional::get)));
+    }
+
+    public static Map<String, Person> groupingByStatusOrderByAgeDescV2(List<Person> people) {
+        return people.stream()
+                .collect(
+                        Collectors.toMap(
+                                Person::getStatus,
+                                Function.identity(),
+                                BinaryOperator.maxBy(Comparator.comparing(Person::getAge))));
+    }
+
+}
---
+++ src/test/java/my/work/stream/terminal/groupingBy/twoparameters/sort/PersonGroupByTwoWithDescOrderTest.java
@@ -0,0 +1,110 @@
+package my.work.stream.terminal.groupingBy.twoparameters.sort;
+
+import my.work.Person;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonGroupByTwoWithDescOrderTest {
+
+    @ParameterizedTest
+    @MethodSource("my.work.PersonStorage#getPeople")
+    void whenCallGroupingByStatusOrderByAgeDescOptional_thenGroupByStatus(List<Person> people) {
+        var expected = Map.of(
+                "high-school",
+                Optional.of(
+                        Person.builder()
+                                .name("Eugen E.")
+                                .age(17)
+                                .status("high-school")
+                                .phones(List.of("*51"))
+                                .build()),
+                "student",
+                Optional.of(Person.builder()
+                        .name("John J.")
+                        .age(20)
+                        .status("student")
+                        .phones(List.of("*10", "*11", "*12"))
+                        .build()),
+                "employee",
+                Optional.of(
+                        Person.builder()
+                                .name("Marry M.")
+                                .age(32)
+                                .status("employee")
+                                .phones(List.of("*31"))
+                                .build()));
+
+        var actual = PersonGroupByTwoWithDescOrder.groupingByStatusOrderByAgeDescOptional(people);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("my.work.PersonStorage#getPeople")
+    void whenCallGroupingByStatusOrderByAgeDescV1_thenGroupByStatus(List<Person> people) {
+        var expected = Map.of(
+                "high-school",
+                Person.builder()
+                        .name("Eugen E.")
+                        .age(17)
+                        .status("high-school")
+                        .phones(List.of("*51"))
+                        .build(),
+                "student",
+                Person.builder()
+                        .name("John J.")
+                        .age(20)
+                        .status("student")
+                        .phones(List.of("*10", "*11", "*12"))
+                        .build(),
+                "employee",
+                Person.builder()
+                        .name("Marry M.")
+                        .age(32)
+                        .status("employee")
+                        .phones(List.of("*31"))
+                        .build());
+
+        var actual = PersonGroupByTwoWithDescOrder.groupingByStatusOrderByAgeDescV1(people);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("my.work.PersonStorage#getPeople")
+    void whenCallGroupingByStatusOrderByAgeDescV2_thenGroupByStatus(List<Person> people) {
+        var expected = Map.of(
+                "high-school",
+                Person.builder()
+                        .name("Eugen E.")
+                        .age(17)
+                        .status("high-school")
+                        .phones(List.of("*51"))
+                        .build(),
+                "student",
+                Person.builder()
+                        .name("John J.")
+                        .age(20)
+                        .status("student")
+                        .phones(List.of("*10", "*11", "*12"))
+                        .build(),
+                "employee",
+                Person.builder()
+                        .name("Marry M.")
+                        .age(32)
+                        .status("employee")
+                        .phones(List.of("*31"))
+                        .build());
+
+        var actual = PersonGroupByTwoWithDescOrder.groupingByStatusOrderByAgeDescV2(people);
+
+        assertEquals(expected, actual);
+    }
+
+}
