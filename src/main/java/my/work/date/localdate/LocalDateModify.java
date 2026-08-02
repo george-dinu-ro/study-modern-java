76fa100ea3aed3700364cfdc4d1eb5ac38116625
george-dinu-ro <george.dinu.ro@gmail.com>
2026-08-02 11:03:52 +0300

    add LocalDate modify examples

---
+++ src/main/java/my/work/date/localdate/LocalDateModify.java
@@ -0,0 +1,33 @@
+package my.work.date.localdate;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+
+public class LocalDateModify {
+
+    public LocalDate plusDays(LocalDate date, int days) {
+        return date.plusDays(days);
+    }
+
+    public LocalDate minusDays(LocalDate date, int days) {
+        return date.minusDays(days);
+    }
+
+    public LocalDate plusMonths(LocalDate date, int months) {
+        return date.plusMonths(months);
+    }
+
+    public LocalDate withYear(LocalDate date, int year) {
+        return date.withYear(year);
+    }
+
+    public LocalDate withYearWithChronoField(LocalDate date, int year) {
+        return date.with(ChronoField.YEAR, year);
+    }
+
+    public LocalDate withTemporalAdjusters(LocalDate date) {
+        return date.with(TemporalAdjusters.firstDayOfMonth());
+    }
+
+}
---
+++ src/test/java/my/work/date/localdate/LocalDateModifyTest.java
@@ -0,0 +1,53 @@
+package my.work.date.localdate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class LocalDateModifyTest {
+
+    private static LocalDateModify localDateModify;
+
+    @BeforeEach
+    void init() {
+        localDateModify = new LocalDateModify();
+    }
+
+    @Test
+    void testPlusDays() {
+        var localDate = localDateModify.plusDays(LocalDate.now(), 10);
+        System.out.println(localDate);
+    }
+
+    @Test
+    void testMinusDays() {
+        var localDate = localDateModify.minusDays(LocalDate.now(), 10);
+        System.out.println(localDate);
+    }
+
+    @Test
+    void testPlusMonths() {
+        var localDate = localDateModify.plusMonths(LocalDate.now(), 2);
+        System.out.println(localDate);
+    }
+
+    @Test
+    void testWithYear() {
+        var localDate = localDateModify.withYear(LocalDate.now(), 2000);
+        System.out.println(localDate);
+    }
+
+    @Test
+    void testWithYearWithChronoField() {
+        var localDate = localDateModify.withYearWithChronoField(LocalDate.now(), 2000);
+        System.out.println(localDate);
+    }
+
+    @Test
+    void testWithTemporalAdjusters() {
+        var localDate = localDateModify.withTemporalAdjusters(LocalDate.now());
+        System.out.println(localDate);
+    }
+
+}
