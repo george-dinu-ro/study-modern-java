4de4f747c31c1bd4c5c281e22857fd7def031ace
george-dinu-ro <george.dinu.ro@gmail.com>
2026-08-02 10:21:35 +0300

    add LocalDate more exmaples

--- src/main/java/my/work/date/localdate/LocalDateSimple.java
+++ src/main/java/my/work/date/localdate/LocalDateSimple.java
@@ -1,11 +1,46 @@
 package my.work.date.localdate;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
 
 public class LocalDateSimple {
 
-    public LocalDate getLocalDate() {
+    public LocalDate now() {
         return LocalDate.now();
+    }
+
+    public LocalDate of(int year, int month, int day) {
+        return LocalDate.of(year, month, day);
+    }
+
+    public LocalDate ofYearDay(int year, int dayOfYear) {
+        return LocalDate.ofYearDay(year, dayOfYear);
+    }
+
+    public Month getMonth(LocalDate date) {
+        return date.getMonth();
+    }
+
+    public int getMonthValue(LocalDate date) {
+        return date.getMonthValue();
+    }
+
+    public DayOfWeek getDayOfWeek(LocalDate date) {
+        return date.getDayOfWeek();
+    }
+
+    public int getDayOfMonth(LocalDate date) {
+        return date.getDayOfMonth();
+    }
+
+    public int getDayOfMonthWithChronoField(LocalDate date) {
+        return date.get(ChronoField.DAY_OF_MONTH);
+    }
+
+    public int getDayOfYear(LocalDate date) {
+        return date.getDayOfYear();
     }
 
 }
--- src/test/java/my/work/date/localdate/LocalDateSimpleTest.java
+++ src/test/java/my/work/date/localdate/LocalDateSimpleTest.java
@@ -3,6 +3,8 @@ package my.work.date.localdate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 class LocalDateSimpleTest {
 
     private static LocalDateSimple localDateSimple;
@@ -13,9 +15,57 @@ class LocalDateSimpleTest {
     }
 
     @Test
-    void testGetLocalDate() {
-        var localDate = localDateSimple.getLocalDate();
-        System.out.println(localDate.toString());
+    void testNow() {
+        var localDate = localDateSimple.now();
+        System.out.println(localDate);
+    }
+
+    @Test
+    void testOf() {
+        var localDate = localDateSimple.of(2026, 8, 2);
+        System.out.println(localDate);
+    }
+
+    @Test
+    void testOfYearDay() {
+        var localDate = localDateSimple.ofYearDay(2026, 214);
+        System.out.println(localDate);
+    }
+
+    @Test
+    void testGetMonth() {
+        var month = localDateSimple.getMonth(LocalDate.now());
+        System.out.println(month);
+    }
+
+    @Test
+    void testGetMonthValue() {
+        var monthValue = localDateSimple.getMonthValue(LocalDate.now());
+        System.out.println(monthValue);
+    }
+
+    @Test
+    void testGetDayOfWeek() {
+        var dayOfWeek = localDateSimple.getDayOfWeek(LocalDate.now());
+        System.out.println(dayOfWeek);
+    }
+
+    @Test
+    void testGetDayOfMonth() {
+        var dayOfMonth = localDateSimple.getDayOfMonth(LocalDate.now());
+        System.out.println(dayOfMonth);
+    }
+
+    @Test
+    void testGetDayOfMonthWithChronoField() {
+        var dayOfMonth = localDateSimple.getDayOfMonthWithChronoField(LocalDate.now());
+        System.out.println(dayOfMonth);
+    }
+
+    @Test
+    void testGetDayOfYear() {
+        var dayOfYear = localDateSimple.getDayOfYear(LocalDate.now());
+        System.out.println(dayOfYear);
     }
 
 }
